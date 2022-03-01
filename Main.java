cba11ce3ef5a34da3745cf31060a845f3ddb1b8e
nirav-dashtechinc <75743304+nirav-dashtechinc@users.noreply.github.com>
2022-03-01 11:44:43 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main {
   public static void main(String args[]) {
     int a = 10, b = 25;
     int c = a + b;
+    
+    System.out.println("Output");
     System.out.println("Addition: " + c);
   }
 }
